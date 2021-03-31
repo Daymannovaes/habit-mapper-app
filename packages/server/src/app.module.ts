9133b42c2097586be05be0f40dc6826982616db6
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-03-31 10:38:42 -0300

    refactor/move configuration files to infrastructure

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '@habit-mapper-app/infrastructure/database/config/ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(ormConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
