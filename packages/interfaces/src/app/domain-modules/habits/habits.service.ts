caab7d8271d7be5c34d71183eef89074b5cf6c7d
Dayman Novaes <daymannovaes@gmail.com>
2021-04-01 02:59:08 -0300

    interfaces: scaffold habitService and CRUD methods

--- packages/interfaces/src/app/domain-modules/habits/habits.service.ts
+++ packages/interfaces/src/app/domain-modules/habits/habits.service.ts
@@ -9,7 +9,22 @@ export class HabitsService {
   constructor(private http: HttpClient) {}
 
   async create(habit: Habit): Promise<Habit> {
-    await this.http.get('http://localhost:3001');
-    return habit;
+    return this.http.post<Habit>('/api/habits', habit).toPromise();
+  }
+
+  findAll() {
+    return this.http.get<Habit[]>(`api/habits`).toPromise();
+  }
+
+  findOne(id: string) {
+    return this.http.get<Habit>(`api/habits/${id}`).toPromise();
+  }
+
+  update(id: number, habit: Habit) {
+    return this.http.patch<Habit>(`api/habits/${id}`, Habit).toPromise();
+  }
+
+  remove(id: string) {
+    return this.http.delete<boolean>(`api/habits/${id}`).toPromise();
   }
 }
