091e84ad301dd1e5256825530fd0cd54b105892a
Dayman Novaes <daymannovaes@gmail.com>
2021-04-01 02:59:08 -0300

    interfaces: scaffold habitService and CRUD methods

--- packages/interfaces/src/app/domain-modules/user/user.service.ts
+++ packages/interfaces/src/app/domain-modules/user/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Auth as AwsAuth } from 'aws-amplify';
+// import { AuthClass } from '@aws-amplify/auth/lib-esm/Auth';
+import { CognitoUserSession } from 'amazon-cognito-identity-js';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +14,13 @@ export class UserService {
   // private provider: AuthClass;
   private userProvider;
 
-  async getUserSession() {
+  async getUserToken(): Promise<string | undefined> {
+    const session = await this.getUserSession();
+
+    return session ? session.getIdToken().getJwtToken() : undefined;
+  }
+
+  async getUserSession(): Promise<CognitoUserSession | undefined> {
     try {
       return await this.userProvider.currentSession();
     } catch (_) {
--- packages/interfaces/src/app/pages/habits/habits.page.ts
+++ packages/interfaces/src/app/pages/habits/habits.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { Habit } from '@habit-mapper-app/entities';
+import { HabitsService } from '../../domain-modules/habits/habits.service';
 
 @Component({
   selector: 'app-habits',
@@ -8,18 +9,19 @@ import { Habit } from '@habit-mapper-app/entities';
   styleUrls: ['./habits.page.scss'],
 })
 export class HabitsPage implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private habitService: HabitsService) {}
 
   habits: Habit[] = [];
 
-  ngOnInit() {
-    this.router.events.subscribe((val) => {
+  async ngOnInit() {
+    this.habits = await this.habitService.findAll();
+    this.router.events.subscribe(async (val) => {
       // @todo use observable filters
       if (val instanceof NavigationEnd) {
         const { state } = this.router.getCurrentNavigation().extras;
 
         if (state && state.newHabit) {
-          this.habits.push(state.newHabit);
+          this.habits = await this.habitService.findAll();
         }
       }
     });
