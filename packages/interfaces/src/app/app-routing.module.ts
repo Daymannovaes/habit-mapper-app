65a1d0cdd8022f2b17d5c18323d50ca0ce5c5b2c
Dayman Novaes <daymannovaes@gmail.com>
2021-04-05 15:31:24 -0300

    interfaces: load habit in edit page and add localstorage for onboarding

--- packages/interfaces/src/app/app-routing.module.ts
+++ packages/interfaces/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './domain-modules/authentication/auth.guard';
+import { OnboardingGuard } from './domain-modules/authentication/onboarding.guard';
 import { NoAuthGuard } from './domain-modules/authentication/no-auth.guard';
 import { NewSessionPage } from './pages/user/new-session.page';
+import { OnboardingPage } from './pages/onboarding/onboarding.page';
 
 const routes: Routes = [
   {
@@ -19,8 +21,8 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'onboarding',
-    pathMatch: 'full',
+    canActivate: [OnboardingGuard],
+    component: OnboardingPage,
   },
   {
     path: 'habits',
--- packages/interfaces/src/app/pages/onboarding/do/onboarding-do.page.ts
+++ packages/interfaces/src/app/pages/onboarding/do/onboarding-do.page.ts
@@ -8,5 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class OnboardingDoPage implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    localStorage.setItem('habit-mapper-app-user-onboarded', 'true');
+  }
 }
