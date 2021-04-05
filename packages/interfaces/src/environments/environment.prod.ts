3f0f21e3b6ff0d17b81d7f5c985d0d1cb65761ea
Dayman Novaes <daymannovaes@gmail.com>
2021-04-05 00:11:00 -0300

    change api endpoint to https

--- packages/interfaces/src/environments/environment.prod.ts
+++ packages/interfaces/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
 
   apiProxyPrefix: '/api',
-  apiFullUrl: 'http://api.knowyourhabit.com',
+  apiFullUrl: 'https://api.knowyourhabit.com',
   // apiFullUrl: 'http://54.198.94.93',
   // apiFullUrl: process.env.API_URL,
 };
