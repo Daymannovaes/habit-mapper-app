6ed74c0c1ddfe2c42eefad7c7fca00459150fd91
Dayman Novaes <daymannovaes@gmail.com>
2021-04-30 14:57:58 -0300

    fix user-interfaces in new folder structure

--- user-interfaces/src/infrastructure.ts
+++ user-interfaces/src/infrastructure.ts
@@ -1,6 +1,6 @@
 // @todo think of a better name for this file
 import Amplify from 'aws-amplify';
-import awsExports from '@habit-mapper-app/infrastructure/aws';
+import awsExports from '@habit-mapper-app/infrastructures/aws';
 
 function configureAwsAmplify() {
   Amplify.configure(awsExports);
